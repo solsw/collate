efd6ec8010e5709e50b0c725eafe86bd8e92be15
Oleg Subachev <git@solsw.ru>
2023-09-22 16:02:08 +0500

    StringOrder updated

--- stringorder.go
+++ stringorder.go
@@ -6,7 +6,7 @@ import (
 )
 
 // StringOrder type implements the [Equaler], [Lesser] and [Comparer] interfaces
-// for [string]s modified according to the mapping function.
+// for [string]s modified according to the mapping function, if any.
 //
 // [string]: https://go.dev/ref/spec#String_types
 type StringOrder struct {
@@ -15,17 +15,28 @@ type StringOrder struct {
 
 // Equal implements the [Equaler] interface.
 func (so StringOrder) Equal(x, y string) bool {
-	return so.Mapping(x) == so.Mapping(y)
+	if so.Mapping != nil {
+		return so.Mapping(x) == so.Mapping(y)
+	}
+	return x == y
 }
 
 // Less implements the [Lesser] interface.
 func (so StringOrder) Less(x, y string) bool {
-	return cmp.Less(so.Mapping(x), so.Mapping(y))
+	if so.Mapping != nil {
+		x = so.Mapping(x)
+		y = so.Mapping(y)
+	}
+	return cmp.Less(x, y)
 }
 
 // Compare implements the [Comparer] interface.
 func (so StringOrder) Compare(x, y string) int {
-	return strings.Compare(so.Mapping(x), so.Mapping(y))
+	if so.Mapping != nil {
+		x = so.Mapping(x)
+		y = so.Mapping(y)
+	}
+	return strings.Compare(x, y)
 }
 
 // CaseInsensitiveOrder is a case-insensitive [StringOrder].
